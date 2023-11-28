dd3134a7c961aba19786783b29701d9f7ae09982
Himanshika Rawat <himanshirawat604@gmail.com>
2023-11-28 23:14:46 +0530

    book library backend

---
+++ app.js
@@ -0,0 +1,107 @@
+// app.js
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// MongoDB connection
+mongoose.connect('mongodb+srv://thakurnakul119:qwertyuiop@cluster0.vppw2ez.mongodb.net/', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Define Book schema and model
+const bookSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  genre: String,
+});
+
+const Book = mongoose.model('Book', bookSchema);
+
+// Define RESTful APIs
+// GET all books
+app.get('/api/books', async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.json(books);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// GET one book by ID
+app.get('/api/books/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const book = await Book.findById(id);
+    if (!book) {
+      return res.status(404).json({ error: 'Book not found' });
+    }
+
+    res.json(book);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// POST add a new book
+app.post('/api/books', async (req, res) => {
+  const { title, author, genre } = req.body;
+
+  try {
+    const newBook = new Book({ title, author, genre });
+    const savedBook = await newBook.save();
+    res.json(savedBook);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// PUT update an existing book
+app.put('/api/books/:id', async (req, res) => {
+  const { id } = req.params;
+  const { title, author, genre } = req.body;
+
+  try {
+    const updatedBook = await Book.findByIdAndUpdate(
+      id,
+      { title, author, genre },
+      { new: true }
+    );
+
+    if (!updatedBook) {
+      return res.status(404).json({ error: 'Book not found' });
+    }
+
+    res.json(updatedBook);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// DELETE remove a book
+app.delete('/api/books/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedBook = await Book.findByIdAndDelete(id);
+
+    if (!deletedBook) {
+      return res.status(404).json({ error: 'Book not found' });
+    }
+
+    res.json(deletedBook);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
